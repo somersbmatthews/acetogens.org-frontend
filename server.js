434fc5fb5962383df62f225cf58ba2a98a33fb66
Somers Matthews <somersbmatthews@uchicago.edu>
2018-05-29 23:55:01 -0500

    added app.get and app.listen to server.js

--- server.js
+++ server.js
@@ -8,3 +8,26 @@ const bodyParser = require('body-parser');
 
 // import method override
 const methodOverride = require('method-override')
+//  implement body parser to use req.body later
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+// connects mongoose
+//mongoose.connect("mongodb://localhost:27017/Authors")
+// middleware that uses method-override module to allow things not allowed in HTML5
+app.use(methodOverride('_method'));
+// middleware that uses express.static to share 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	// res.render('index.ejs')
+	res.send("this is your site")
+})
+
+app.get('*', (req, res) => {
+	res.send("error, route is wrong")
+})
+
+app.listen(3001, () => {
+	console.log("listening on port " + 3001)
+})
