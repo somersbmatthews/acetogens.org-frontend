d9c0b598e3cd5073fe4264c252a35770fd38dd00
Somers Matthews <somersbmatthews@uchicago.edu>
2018-05-29 23:52:39 -0500

    added middleware and port number to server.js file

---
+++ server.js
@@ -0,0 +1,10 @@
+const port = 3001;
+
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// import method override
+const methodOverride = require('method-override')
